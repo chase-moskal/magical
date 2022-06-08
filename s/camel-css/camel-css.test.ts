c4566ebdb868bb9c98ed4afa12b14c59cb1d93ef
Chase Moskal <chasemoskal@gmail.com>
2022-06-08 14:28:01 -0700

    implement: flat parser

--- s/camel-css/camel-css.test.ts
+++ s/camel-css/camel-css.test.ts
@@ -2,6 +2,7 @@
 import {Suite, expect} from "cynic"
 import {Token} from "./parsing/ordinary/types.js"
 import {tokenize} from "./parsing/ordinary/tokenize.js"
+import {parse} from "./parsing/ordinary/parse.js"
 
 const source = `
 	header {
@@ -13,32 +14,61 @@ const source = `
 
 export default <Suite>{
 	"ordinary syntax": {
-		"tokenize": {
-			async "returns the correct number of tokens"() {
-				const tokens = tokenize(source)
-				expect(tokens.length).equals(8)
-			},
-			async "returns the correct tokens"() {
-				const correctTokenTypes = [
-					Token.Type.Selector,
-					Token.Type.Open,
-					Token.Type.Selector,
-					Token.Type.Open,
-					Token.Type.RuleName,
-					Token.Type.RuleValue,
-					Token.Type.Close,
-					Token.Type.Close,
-				]
-				const tokens = tokenize(source)
-				expect(tokens.length).equals(correctTokenTypes.length)
-				const correct = correctTokenTypes
-					.every((type, index) => tokens[index].type === type)
-				expect(correct).ok()
-			},
-		},
+		// "tokenize": {
+		// 	async "returns the correct number of tokens"() {
+		// 		const tokens = tokenize(source)
+		// 		expect(tokens.length).equals(8)
+		// 	},
+		// 	async "returns the correct tokens"() {
+		// 		const correctTokenTypes = [
+		// 			Token.Type.Selector,
+		// 			Token.Type.Open,
+		// 			Token.Type.Selector,
+		// 			Token.Type.Open,
+		// 			Token.Type.RuleName,
+		// 			Token.Type.RuleValue,
+		// 			Token.Type.Close,
+		// 			Token.Type.Close,
+		// 		]
+		// 		const tokens = tokenize(source)
+		// 		expect(tokens.length).equals(correctTokenTypes.length)
+		// 		const correct = correctTokenTypes
+		// 			.every((type, index) => tokens[index].type === type)
+		// 		expect(correct).ok()
+		// 	},
+		// },
 		"parse": {
-			async ""() {
-				
+			async "flat source code into expressions"() {
+				const tokens = tokenize(`
+					h1 {
+						color: red;
+						text-align: center;
+					}
+					h2 {
+						font-style: italic;
+					}
+				`)
+				const expressions = parse(tokens)
+				expect(expressions.length).equals(2)
+			},
+			async "nested source code into nested expressions"() {
+				const tokens = tokenize(`header { h1 { color: red; } }`)
+				const expressions = parse(tokens)
+				expect(expressions.length).equals(1)
+				{
+					const [expression1] = expressions
+					const [selector, rules, children] = expression1
+					expect(selector).equals("header")
+					expect(Object.keys(rules).length).equals(0)
+					expect(children.length).equals(1)
+					{
+						const [child] = children
+						const [selector, rules, children2] = child
+						expect(selector).equals("h1")
+						expect(Object.keys(rules).length).equals(1)
+						expect(children2.length).equals(0)
+					}
+				}
 			},
 		},
 	},
--- s/camel-css/parsing/ordinary/parse.ts
+++ s/camel-css/parsing/ordinary/parse.ts
@@ -4,5 +4,45 @@ import {Expression} from "../../types.js"
 
 export function parse(tokens: Token.Any[]): Expression[] {
 	const expressions: Expression[] = []
+
+	if (tokens.length > 0 && tokens[0].type !== Token.Type.Selector)
+		throw new Error("file must start with a selector!")
+
+	let selector: undefined | string
+	let ruleName: undefined | string
+	let rules: {[key: string]: string} = {}
+
+	for (const token of tokens) {
+		switch (token.type) {
+
+			case Token.Type.Selector: {
+				selector = token.value
+			} break
+
+			case Token.Type.Open: {} break
+
+			case Token.Type.RuleName: {
+				ruleName = token.value
+			} break
+
+			case Token.Type.RuleValue: {
+				if (ruleName)
+					rules[ruleName] = token.value
+				else
+					throw new Error("rule value cannot precede rule name")
+				ruleName = undefined
+			} break
+
+			case Token.Type.Close: {
+				if (!selector)
+					throw Error("expression must have a selector!")
+				expressions.push([selector, rules, []])
+				selector = undefined
+				ruleName = undefined
+				rules = {}
+			} break
+		}
+	}
+
 	return expressions
 }
