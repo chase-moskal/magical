f9f61d05bcb3841c5f19f5597a74b8b87bfa9eea
Paul_Aroo <timmypaul2006@gmail.com>
2022-06-09 13:22:56 +0100

    add: demo stage for camel-css

--- s/internal/demo/my-demo.ts
+++ s/internal/demo/my-demo.ts
@@ -1,8 +1,52 @@
 
-import {LitElement, html} from "lit"
+import {LitElement, html, css} from "lit"
+import {property} from 'lit/decorators.js';
+import {compile} from "../../camel-css/compilation/compile.js"
+import {parse} from "../../camel-css/parsing/ordinary/parse.js"
+import {tokenize} from "../../camel-css/parsing/ordinary/tokenize.js"
 import {ExampleCounter} from "../../component/examples/example-counter.js"
 
 export class MyDemo extends LitElement {
+
+	static styles = css`
+
+		div {
+			display: flex;
+			justify-content: space-between;
+			padding: 1rem 0;
+		}
+
+		textarea {
+			width: 12rem;
+			height: 14rem;
+			background: transparent;
+			color: white;
+			padding: 0.2rem;
+			border-radius: 5px;
+		}
+  `;
+
+	@property()
+	private demoInput: string = `
+	header {
+		color: red;
+		h1 {
+			font-size: 1rem;
+		}
+	}`
+
+	@property()
+	private demoOutput: string = ""
+
+	#handleInputChange = (event: Event) => {
+		const input = event.target as HTMLTextAreaElement
+		// this.demoOutput = input.value
+
+		const tokens = tokenize(input.value)
+		const expressions = parse(tokens)
+		const css = compile(expressions)
+		this.demoOutput = css
+	}
 
 	render() {
 		return html`
@@ -10,6 +54,11 @@ export class MyDemo extends LitElement {
 			<p>here is an example component:</p>
 			<br/>
 			${ExampleCounter(0)}
+
+			<div>
+				<textarea style="width: 12rem; height:14rem" @input=${this.#handleInputChange}></textarea>
+				<textarea readonly>${this.demoOutput}</textarea>
+			</div>
 		`
 	}
 }
