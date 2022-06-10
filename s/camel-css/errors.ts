c1bd751fe406b38a2bf52311e23f08bbc4641019
Chase Moskal <chasemoskal@gmail.com>
2022-06-10 12:17:02 -0700

    fix: test by implementing missing brace error

--- s/camel-css/errors.ts
+++ s/camel-css/errors.ts
@@ -3,30 +3,50 @@ import {Trace} from "./parsing/ordinary/types.js"
 
 export class CamelCssError extends Error {
 	name = this.constructor.name
+}
+
+//
+// untracable errors
+//
+
+export class CamelCssMissingClosingBraceError extends CamelCssError {
+	constructor(missingCloses: number) {
+		const message = missingCloses > 1
+			? `missing ${missingCloses} closing braces "}"}`
+			: `missing ${missingCloses} closing brace "}"}`
+		super(message)
+	}
+}
+
+//
+// traceable errors
+//
+
+export class CamelCssTraceableError extends CamelCssError {
 	constructor(public trace: Trace, message: string) {
 		super(`(${trace.cursor.line}:${trace.cursor.column}) ${message}`)
 	}
 }
 
-export class CamelCssMissingSelectorError extends CamelCssError {
+export class CamelCssMissingSelectorError extends CamelCssTraceableError {
 	constructor(trace: Trace) {
 		super(trace, `expression is missing selector`)
 	}
 }
 
-export class CamelCssStackError extends CamelCssError {
+export class CamelCssExcessClosingBraceError extends CamelCssTraceableError {
 	constructor(trace: Trace) {
-		super(trace, `unexpected closing brace "}"`)
+		super(trace, `unwanted closing brace "}"`)
 	}
 }
 
-export class CamelCssRuleNamePlacementError extends CamelCssError {
+export class CamelCssRuleNamePlacementError extends CamelCssTraceableError {
 	constructor(trace: Trace, ruleName: string) {
 		super(trace, `invalid place for rule name "${ruleName}"`)
 	}
 }
 
-export class CamelCssRuleValuePlacementError extends CamelCssError {
+export class CamelCssRuleValuePlacementError extends CamelCssTraceableError {
 	constructor(trace: Trace, ruleValue: string) {
 		super(trace, `invalid place for rule value "${ruleValue}"`)
 	}
@@ -34,13 +54,13 @@ export class CamelCssRuleValuePlacementError extends CamelCssError {
 
 export const setupTracedErrors = (trace: Trace) => ({
 	error: (message: string) =>
-		new CamelCssError(trace, message),
+		new CamelCssTraceableError(trace, message),
 
 	missingSelector: () =>
 		new CamelCssMissingSelectorError(trace),
 
-	stack: () =>
-		new CamelCssStackError(trace),
+	excessClosingBrace: () =>
+		new CamelCssExcessClosingBraceError(trace),
 
 	ruleNamePlacement: (ruleName: string) =>
 		new CamelCssRuleNamePlacementError(trace, ruleName),
