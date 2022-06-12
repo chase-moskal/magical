09f513fcf6a279efebd8618a7c7413f7357f5103
Chase Moskal <chasemoskal@gmail.com>
2022-06-11 22:08:19 -0700

    rename: internal term 'component' to 'element'

--- s/theme-elements.ts
+++ s/theme-elements.ts
@@ -6,12 +6,12 @@ import {mixinStyles} from "./mixins/mixin-styles.js"
 import {Constructor} from "./toolbox/handy-types.js"
 
 export const themeElements = <
-		xComponents extends {[key: string]: Constructor<LitElement>}
+		xElements extends {[key: string]: Constructor<LitElement>}
 	>(
 		theme: CSSResultGroup,
-		components: xComponents,
+		elements: xElements,
 	) => {
 
-	return obtool(components)
-		.map(Component => mixinStyles(theme)(Component))
+	return obtool(elements)
+		.map(Element => mixinStyles(theme)(Element))
 }
