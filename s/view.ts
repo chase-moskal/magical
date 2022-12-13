fb95a5ff9ff655ddb0097d3ab83475509de88486
Chase Moskal <chasemoskal@gmail.com>
2022-12-13 15:00:10 -0800

    refactor: view, generate use only once

--- s/view.ts
+++ s/view.ts
@@ -3,11 +3,9 @@ import {render} from "lit"
 import {directive, Part} from "lit/directive.js"
 import {AsyncDirective} from "lit/async-directive.js"
 
-import {debounce} from "./toolbox/debounce/debounce.js"
-import {createStateSetter} from "./view/create-state-setter.js"
-import {initializeAndGetState} from "./view/initialize-and-get-state.js"
+import {makeUseObjectForView} from "./view/make-use-object-for-view.js"
+import {View, Sauce, SetupMap, StateMap, ViewOptions} from "./view/types.js"
 import {createShadowDomWithStyles} from "./view/create-shadow-dom-with-styles.js"
-import {View, Sauce, SetupMap, StateMap, UseView, ViewOptions} from "./view/types.js"
 
 export const view = <xProps extends any[]>(
 		{styles, shadow = false}: ViewOptions,
@@ -16,55 +14,24 @@ export const view = <xProps extends any[]>(
 
 	class ViewDirective extends AsyncDirective {
 		#mostRecentProps = <xProps><unknown>[]
+
 		#stateMap: StateMap = new Map<number, [any, any]>()
 		#setupMap: SetupMap = new Map<number, () => void>()
 
-		#generateUse(): UseView {
-			const stateMap = this.#stateMap
-			const setupMap = this.#setupMap
-			let stateIndex = 0
-			let setupIndex = 0
-			const rerender = debounce(
-				0,
-				() => this.setValue(this.#renderIntoShadowOrNot()),
-			)
-			return {
+		#stateIndex = 0
+		#setupIndex = 0
+		#renderCount = 0
 
-				state(initial) {
-					const [currentValue, previousValue] = initializeAndGetState({
-						initial,
-						stateIndex,
-						stateMap,
-					}) ?? []
-
-					const setter = createStateSetter<any>({
-						stateMap,
-						stateIndex,
-						rerender,
-					})
-
-					const getter = () => (stateMap.get(stateIndex) ?? [])[0]
-
-					stateIndex += 1
-
-					return [
-						currentValue,
-						setter,
-						getter,
-						previousValue,
-					]
-				},
-
-				setup(e) {
-					const initialized = setupMap.has(setupIndex)
-
-					if (!initialized)
-						setupMap.set(setupIndex, e(rerender))
-
-					setupIndex += 1
-				},
-			}
-		}
+		#use = makeUseObjectForView({
+			stateMap: this.#stateMap,
+			setupMap: this.#setupMap,
+			pullStateIndex: () => this.#stateIndex++,
+			pullSetupIndex: () => this.#setupIndex++,
+			getRenderCount: () => this.#renderCount,
+			render: () => {
+				this.setValue(this.#renderIntoShadowOrNot())
+			},
+		})
 
 		#root = shadow
 			? createShadowDomWithStyles(styles)
@@ -86,17 +53,16 @@ export const view = <xProps extends any[]>(
 
 		disconnected() {
 			super.disconnected()
-
+			this.#renderCount = 0
 			for (const dispose of this.#setupMap.values())
 				dispose()
-
 			this.#setupMap.clear()
-			this.#stateMap.clear()
 		}
 
 		render() {
-			const use = this.#generateUse()
-			const renderer = sauce(use)
+			this.#stateIndex = 0
+			this.#setupIndex = 0
+			const renderer = sauce(this.#use)
 			return renderer(...this.#mostRecentProps)
 		}
 	}
