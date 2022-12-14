eba4e277f4f1b222e02d943fd41cb2345179d72c
Chase Moskal <chasemoskal@gmail.com>
2022-12-14 00:23:36 -0800

    tweak: mixins, lightdom signature

--- s/mixins/mixin-light-dom.ts
+++ s/mixins/mixin-light-dom.ts
@@ -2,13 +2,12 @@
 import {LitElement} from "lit"
 import {Constructor} from "../toolbox/handy-types.js"
 
-export function mixinLightDom<
-		C extends Constructor<LitElement>
-	>(Base: C): C {
-
-	return class extends Base {
-		createRenderRoot() {
-			return this
+export function mixinLightDom() {
+	return function<C extends Constructor<LitElement>>(Base: C): C {
+		return class extends Base {
+			createRenderRoot() {
+				return this
+			}
 		}
 	}
 }
